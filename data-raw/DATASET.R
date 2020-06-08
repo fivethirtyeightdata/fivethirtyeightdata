80fcbdf514018cc33c8d08d33b7dad8124471ff2
mariumtapal <mariumtapal@gmail.com>
2020-06-08 12:57:38 +0500

    add dataset

---
+++ data-raw/DATASET.R
@@ -0,0 +1,14 @@
+## code to prepare `DATASET` dataset goes here
+library(dplyr)
+library(tidyr)
+library(readr)
+url <- "https://raw.githubusercontent.com/fivethirtyeight/data/master/twitter-ratio/senators.csv"
+senators <- read_csv(url) %>%
+  mutate(
+    party = as.factor(party),
+    state = as.factor(state),
+    created_at = as.POSIXct(created_at, tz = "GMT", format = "%m/%d/%Y %H:%M"),
+    text =  gsub("[^\x01-\x7F]", "", text)
+  ) %>%
+  select(created_at, user, everything())
+usethis::use_data(senators, overwrite = TRUE)
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(fivethirtyeightdata.test)
+
+test_check("fivethirtyeightdata.test")
